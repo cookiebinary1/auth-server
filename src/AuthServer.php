5b77f10de03e8cf48781df95f96c62cab611d8ab
Martin Osusky <cookie.binary@gmail.com>
2019-02-18 20:07:30 +0100

    update

--- src/AuthServer.php
+++ src/AuthServer.php
@@ -171,6 +171,8 @@ class AuthServer
                 $this->accessTokenJWT();
             } catch (ExpiredException $exception) {
                 $this->refreshAccessToken();
+            } catch (\UnexpectedValueException $exception) {
+                return null;
             }
         }
 
@@ -359,6 +361,15 @@ class AuthServer
         $response = json_decode($response, true) ?? $response;
 
         return $response;
+    }
+
+    /**
+     * @return bool
+     * @author Cookie
+     */
+    public function check()
+    {
+        return (bool)$this->accessToken();
     }
 }
 
